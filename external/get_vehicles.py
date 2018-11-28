01a0b823a51d5379f5181c722c8cb1ca66d494b1
haskone <3598179+haskone@users.noreply.github.com>
2018-11-27 22:58:27 -0500

    Minor updates + saving date as datetime instead of string
    - update requirements
    - try/except in order to guarantee processing for every record
    - save ISO str as datetime (using arrow to parse)

--- external/get_vehicles.py
+++ external/get_vehicles.py
@@ -1,6 +1,8 @@
 import requests
 import json
+import arrow
 
+from collections import defaultdict
 from connect_db import get_db
 from constants import (
     GET_ROUTES_ENDPOINT,
@@ -45,6 +47,19 @@ vehicle_mapping = {
     'Work': 'Work'     
 }
 
+def transform_date(str_date):
+    if not str_date:
+        # sometimes NextStop* returns {}
+        return str_date
+    return arrow.get(str_date).datetime
+
+transform_mapping = defaultdict(lambda: lambda x: x)
+transform_mapping['GpsDate'] = transform_date
+transform_mapping['NextStopTimeToStop'] = transform_date
+transform_mapping['NextStopEstimatedDepartTime'] = transform_date
+transform_mapping['NextStopScheduledWorkDate'] = transform_date
+transform_mapping['NextStopArrivalAtStop'] = transform_date
+
 def get_veh_data(dct, layers):
     if dct is None:
         return {}
@@ -59,24 +74,34 @@ def filter_vehicles(r):
     veh_filtered = []
     for dct_item in r:
         veh_fil = {}
-        for route_key, final_key in mapping.items():
-            veh_fil[final_key] = get_veh_data(
-                dct_item,
-                route_key.split('.') if '.' in route_key else route_key
-            )
-        for pattern_item in dct_item['VehiclesByPattern']:
-            for route_key, final_pattern_key in vehicle_pattern_mapping.items():
-                veh_fil[final_pattern_key] = get_veh_data(
-                    pattern_item,
+        try:
+            for route_key, final_key in mapping.items():
+                veh_fil[final_key] = get_veh_data(
+                    dct_item,
                     route_key.split('.') if '.' in route_key else route_key
                 )
-            for veh_item in pattern_item['Vehicles']:
-                for veh_key, final_veh_key in vehicle_mapping.items():
-                    veh_fil[final_veh_key] = get_veh_data(
-                        veh_item,
-                        veh_key.split('.') if '.' in veh_key else veh_key
+
+            for pattern_item in dct_item['VehiclesByPattern']:
+                for route_key, final_pattern_key in vehicle_pattern_mapping.items():
+                    veh_fil[final_pattern_key] = get_veh_data(
+                        pattern_item,
+                        route_key.split('.') if '.' in route_key else route_key
                     )
-        veh_filtered.append(veh_fil)
+                for veh_item in pattern_item['Vehicles']:
+                    for veh_key, final_veh_key in vehicle_mapping.items():
+                        veh_fil[final_veh_key] = get_veh_data(
+                            veh_item,
+                            veh_key.split('.') if '.' in veh_key else veh_key
+                        )
+            veh_fil = {k: transform_mapping[k](v) for k, v in veh_fil.items()}
+            veh_filtered.append(veh_fil)
+        except Exception as e:
+            # TODO: use logging
+            print(
+                f'Something went wrong in get_all_vehicles '
+                f'but need to try other items: {e}'
+            )
+
     return veh_filtered 
 
 def get_all_vehicles():
@@ -86,18 +111,32 @@ def get_all_vehicles():
 
     # GetVehicles
     vehicle_array = []
-    for i in range(0, len(route_data)):
-        direction_value = route_data[i]['PatternList'][0]['Direction']['DirectionKey']
-        route_value = route_data[i]['Key']
-        vehicle_params = 'routeDirectionKeys[0][RouteKey]={0}&routeDirectionKeys[0][DirectionKey]={1}'.format(route_value,direction_value)
-        headers = {
-            'Content-Type': "application/x-www-form-urlencoded",
-        }
-        vehicle_r = requests.post(GET_VEHICLES_ENDPOINT, data=vehicle_params, headers=headers)
-        vehicle_data = vehicle_r.json()
-        vehicle_draft = json.dumps(vehicle_data)
-        vehicle = json.loads(vehicle_draft)
-        vehicle_array = vehicle_array + vehicle
+    for route_item in route_data:
+        try:
+            direction_value = route_item['PatternList'][0]['Direction']['DirectionKey']
+            route_value = route_item['Key']
+            vehicle_params = (
+                f'routeDirectionKeys[0][RouteKey]={route_value}&'
+                f'routeDirectionKeys[0][DirectionKey]={direction_value}'
+            )
+            headers = {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            }
+            vehicle_r = requests.post(
+                GET_VEHICLES_ENDPOINT,
+                data=vehicle_params,
+                headers=headers,
+            )
+            vehicle_data = vehicle_r.json()
+            vehicle_draft = json.dumps(vehicle_data)
+            vehicle = json.loads(vehicle_draft)
+            vehicle_array = vehicle_array + vehicle
+        except Exception as e:
+            # TODO: use logging
+            print(
+                f'Something went wrong in get_all_vehicles '
+                f'but need to try other items: {e}'
+            )
 
     return filter_vehicles(vehicle_array)
 
