70133b230a01b867f5a46acf5dd8a546735803fb
Dan Pham <danhanh1992@gmail.com>
2018-12-11 16:22:16 -0500

    change aws link

--- app/utils.py
+++ app/utils.py
@@ -6,5 +6,5 @@ db = None
 def get_db():
     global db
     if db is None:
-        db = DB(url=os.environ.get('bus_dburl', 'mongodb://barrieTransit:BarrieTransit@ec2-18-222-217-102.us-east-2.compute.amazonaws.com:27017')) 
+        db = DB(url=os.environ.get('bus_dburl', 'mongodb://barrieTransit:BarrieTransit@ec2-3-16-169-75.us-east-2.compute.amazonaws.com:27017')) 
     return db
